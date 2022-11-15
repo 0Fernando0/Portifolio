3a123422ad865b8149b95031da966d88aaf9e417
Fernando Borges Pereira <fernandoborgespereira57@gmail.com>
2022-11-15 18:01:58 -0300

    adicinei uma versão melhor

--- Baixar Manga/app.py
+++ Baixar Manga/app.py
@@ -1,13 +1,35 @@
-from BackEnd import Scraping
+from playwright.sync_api import sync_playwright
+import os,requests
 
-manga = input('manga: '.title()).strip().replace(' ','-')
+def app():
+    manga = input("nome do manga: ")  
+    with sync_playwright() as play:
+        nav = play.chromium.launch(channel='chrome',headless=True)
 
-html = Scraping(f'https://muitomanga.com/manga/{manga}')
-print(f'{html.listacapitulos()} capitulos disponiveis'.title())
+        page = nav.new_page(base_url='https://muitomanga.com/')
+        
+        page.goto('manga/'+manga.replace(' ','-'))
+        for capitulo in (page.locator('div.single-chapter>a').all_text_contents()):
+            print(capitulo)
+        cap_selecionado = input('Qual Capitulo Baixar: ')
+        
+        
+        page.goto('ler/'+manga.replace(' ','-')+'/capitulo-'+cap_selecionado)
+        
+        paginas = (page.locator('select.select_paged>option').count())
+        try:
+            os.makedirs(f'Manga/{manga}/{cap_selecionado}')
+        except:
+            pass
+        for c in range(paginas+1):
+            page.locator('a#chimagenext').click()
+            
+            link = (page.locator('img#imagech').get_attribute('src'))
 
-capitulo = input('capitulo: '.title()).strip()
-print('baixando...'.title())
-paginas = Scraping(f'https://muitomanga.com/ler/{manga}/capitulo-{capitulo}')
-paginas.baixar_paginas(manga,capitulo)
+            
+            with open(f'Manga/{manga}/{cap_selecionado}/imagem{c}.jpg','wb') as file:
+                file.write(requests.get(link).content)          
+               
+        nav.close() 
 
-print('download concluido\nboa leitura!'.title())
+app()
