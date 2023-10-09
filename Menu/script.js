a08e83841070c7d8304c6995c642d7045978105b
fernando <fernandoborgespereira57@gmail.com>
2023-10-09 16:07:32 -0300

    feat: remove all projects

--- Menu/script.js
+++
@@ -1,7 +0,0 @@
-function esconder(){
-    // OPERADOR TERNARIO
-    menu.style.display = (menu.style.display == "block"? "none":"block");
-}
-function mudouTamanho(){
-    menu.style.display = (window.innerWidth >= 768 ? "block":"none");
-}
