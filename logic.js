7ce0f5779f3c1087c875931ce9d2b6a660d70df7
0Fernando0 <fernandoborgespereira57@gmail.com>
2024-10-18 01:58:28 -0300

    style adjust

--- logic.js
+++ logic.js
@@ -1,17 +1,30 @@
-let nav = document.querySelector("header>nav");
-
-function alterar(){
-    nav.style.display = nav.style.display == "none" ? "flex" : "none";
+function screenScrol(item){
+    const navLinks = document.querySelectorAll("nav>a");
+    navLinks.forEach((elem) => {
+        elem.classList.remove("active");
+    })
+    item.classList.add("active")
 }
 
-document.body.onload = function(){
-    if(window.innerWidth < 668){
-        nav.style.display = "none";
-    }
+function setMenu(){
+    let menu = document.querySelector("header>nav");
+    menu.style.display = menu.style.display == "none" ? "flex" : "none";
 }
 
-document.body.onresize = function(){
-    if(window.innerWidth > 668){
-        nav.style.display = "flex";
-    }
-}
+// let nav = document.querySelector("header>nav");
+
+// function alterar(){
+//     nav.style.display = nav.style.display == "none" ? "flex" : "none";
+// }
+
+// document.body.onload = function(){
+//     if(window.innerWidth < 668){
+//         nav.style.display = "none";
+//     }
+// }
+
+// document.body.onresize = function(){
+//     if(window.innerWidth > 668){
+//         nav.style.display = "flex";
+//     }
+// }
