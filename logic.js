a3e75fc14dc75c39b950baa39dfb868c2597fec2
0Fernando0 <fernandoborgespereira57@gmail.com>
2024-10-22 03:33:50 -0300

    complete animation header link page

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
 let menu = document.querySelector("header>nav");
+let links_navegacao = document.querySelectorAll("nav>a");
+let sections = document.querySelectorAll("section");
 
 function setDisplayMenu(){
     if(window.innerWidth >= 952){
@@ -13,20 +15,34 @@ function setMenu(){
     menu.style.display = menu.style.display == "none" ? "flex" : "none";
 }
 
-// let nav = document.querySelector("header>nav");
+function animationObserver(){
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                let section_id = entry.target.getAttribute("id");
+                for (const link of links_navegacao) {
+                    let link_id = link.getAttribute("href").slice(1)
+                    if(link_id == section_id){
+                        link.classList.add("visible")
+                        break
+                    }
+                }
+            }
+            else{
+                let section_id = entry.target.getAttribute("id");
+                for (const link of links_navegacao) {
+                    let link_id = link.getAttribute("href").slice(1)
+                    if(link_id == section_id){
+                        link.classList.remove("visible")
+                        break
+                    }
+                }
+            }
+        })
+    })
+    sections.forEach(sec => {
+        observer.observe(sec)
+    })
+}
 
-// function alterar(){
-//     nav.style.display = nav.style.display == "none" ? "flex" : "none";
-// }
-
-// document.body.onload = function(){
-//     if(window.innerWidth < 668){
-//         nav.style.display = "none";
-//     }
-// }
-
-// document.body.onresize = function(){
-//     if(window.innerWidth > 668){
-//         nav.style.display = "flex";
-//     }
-// }
+animationObserver()
